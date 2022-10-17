ebbfde620f328ee9d4e619af7b366e4718caecff
Furkan Cemal Caliskan <furkanc.caliskan@icloud.com>
2022-10-17 14:00:28 +0300

    Initial Commit

---
+++ CNQuotesSwiftUI/CNQuotesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CNQuotesSwiftUIApp.swift
+//  CNQuotesSwiftUI
+//
+//  Created by Furkan Cemal Çalışkan on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CNQuotesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
